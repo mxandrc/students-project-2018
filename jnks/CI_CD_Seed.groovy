0c1fb9af6c959c9b24cbe2ebf3fac6db014be25f
root <root@localhost.localdomain>
2018-06-21 15:49:08 +0300

    Committer: root <root@localhost.localdomain> 	modified:   CI_CD_Seed.groovy 	modified:   CI_job.groovy

--- jnks/CI_CD_Seed.groovy
+++ jnks/CI_CD_Seed.groovy
@@ -1,1 +1,19 @@
-
+pipelineJob("CI-job") {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/mxandrc/students-project-2018.git')
+                        credentials('mxandrc')
+                    }
+                    branch('refs/tags/*.*')
+                }
+            }
+            scriptPath("jnks/CI_job.groovy")
+        }
+    }
+	triggers {
+        scm('H/1 * * * *')
+    }
+}
